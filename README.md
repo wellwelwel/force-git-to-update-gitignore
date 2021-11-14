86f79347401b5caeef5e71805095e7a31b2a9f28
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2021-11-14 00:39:15 -0300

    Done 🚀

---
+++ README.md
@@ -0,0 +1,18 @@
+# **Removes the cache of all files specified in `.gitignore`**
+
+Import or execute directly the module to uncache all files listeds in `.gitignore`.
+<hr />
+
+The follow command is executed for each file:
+
+```shell
+   git rm -r --cached *current-file* --ignore-unmatch
+```
+
+If you just want to run the script, insert in `Terminal` the command:
+
+```shell
+   node index.js # or whatever name what you put
+```
+
+Made by me for you 😁
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-(async () => {
-   
+'use strict';
+
+const refreshGitIgnore = (includeComments = true) => {
+
+   const { execSync } = require('child_process');
+   const { EOL } = require('os');
    const fs = require('fs');
-   // const exec = require('child_process').exec;
-
    const gitignore = fs.readFileSync('.gitignore', 'utf-8');
-   const list = gitignore.split('\n');
+   const list = gitignore.split(EOL);
+   const sh = { green: '\x1b[32m', red: '\x1b[31m', bold: '\x1b[1m', yellow: '\x1b[33m', reset: '\x1b[0m' };
 
-   list.forEach(file => {
+   list.forEach(toIgnore => {
       
-      
+      if (!includeComments && /#/.test(toIgnore)) return;
+
+      const path = toIgnore.replace(/#/, '').trim();
+      const command = `git rm -r --cached ${sh.bold}${sh.yellow}${path}${sh.reset} --ignore-unmatch`;
+
+      try {
+         
+         execSync(command, { stdio: 'inherit' });
+
+         console.log(`${sh.green}✔${sh.reset} ${command}`);
+      }
+      catch (e) {
+         
+         console.log(`${sh.red}✖${sh.reset} ${command} - ${e.message}`);
+      }
    });
-})();
+};
+
+module.exports = refreshGitIgnore;
+
+/* uncomment the follow line to auto execute */
+// refreshGitIgnore();
