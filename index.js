a4b9242fc071d13d54bcbd16df89b8b77de1180c
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2021-11-14 22:45:49 -0300

    Update index.js
    Change to auto execute

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 'use strict';
 
-const refreshGitIgnore = (includeComments = true) => {
+((includeComments = true) => {
 
    const { execSync } = require('child_process');
    const { EOL } = require('os');
    const fs = require('fs');
    const gitignore = fs.readFileSync('.gitignore', 'utf-8');
    const list = gitignore.split(EOL);
-   const sh = { green: '\x1b[32m', red: '\x1b[31m', bold: '\x1b[1m', yellow: '\x1b[33m', reset: '\x1b[0m' };
+   const sh = { green: '\x1b[32m', red: '\x1b[31m', reset: '\x1b[0m' };
 
    list.forEach(toIgnore => {
       
+      if (toIgnore?.trim().length === 0) return;
       if (!includeComments && /#/.test(toIgnore)) return;
 
       const path = toIgnore.replace(/#/, '').trim();
-      const command = `git rm -r --cached ${sh.bold}${sh.yellow}${path}${sh.reset} --ignore-unmatch`;
+      const command = `git rm -r --cached ${path} --ignore-unmatch`;
 
       try {
          
          execSync(command, { stdio: 'inherit' });
 
          console.log(`${sh.green}✔${sh.reset} ${command}`);
-      }
-      catch (e) {
+      } catch (e) {
          
          console.log(`${sh.red}✖${sh.reset} ${command} - ${e.message}`);
       }
    });
-};
-
-module.exports = refreshGitIgnore;
-
-/* uncomment the follow line to auto execute */
-// refreshGitIgnore();
+})();
